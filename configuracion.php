a014b92e12721f3301405f5dbbbb95b248a3b8ec
Enrique <enrique.rios.santos@gmail.com>
2015-10-13 21:38:52 +0200

    Update configuracion.php

--- configuracion.php
+++ configuracion.php
@@ -1,6 +1,6 @@
 <?php
 
 class Configuracion{
-    const URL_BASE = 'http://5.134.118.246:8080/gavWS/webresources/pujador/';
+    const URL_BASE = 'http://[VPS_IP]:8080/gavWS/webresources/pujador/';
     const URL_PUJAR = 'pujar';
 }
