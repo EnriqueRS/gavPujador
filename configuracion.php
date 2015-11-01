0a45d64fd4aedc9193a0f0a243e940a3e6060066
Enrique Rios <enrique@rios.com>
2015-11-01 12:33:22 +0100

    -pujador_cron: A la hora de determinar si una obra está pujada se comprueba mediante el ID del usuario máximo pujador. -Se añade fichero de pruebas para la función pujador_cron

--- configuracion.php
+++ configuracion.php
@@ -1,7 +1,7 @@
 <?php
 
 class Configuracion{
-    const URL_BASE = 'http://[VPS_IP]:8080/gavWS/webresources/pujador/';
+    const URL_BASE = 'http://localhost:54619/gavWS/webresources/pujador/';
     const URL_PUJAR = 'pujar';
     const CLAVE_CIFRADO = "hKdgFG47";
 }
